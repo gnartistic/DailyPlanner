715946b278957696bc0acdcdaa89d0a2c2897cc3
charlie <gn4rtistic@gmail.com>
2022-10-30 08:31:55 -0500

    created type defs and resolvers along with utilities

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require( 'apollo-server-express' );
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    firstName: String
+    lastName: String
+    email: String
+    Tasks: [Task]
+}
+
+type Task {
+    _id: ID
+    taskText: String
+    createdAt: String
+    username: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    task(username: String): [Task]
+    task(_id: ID!): Task
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addTask(taskText: String!): Task
+}
+`;
+
+module.exports = typeDefs;
